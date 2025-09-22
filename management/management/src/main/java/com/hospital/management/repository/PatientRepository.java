4a18e78a6202fb142b3123ac6e640dfa90b3db3d
Vish014 <kaushan-ps21052@stu.kln.ac.lk>
2025-09-22 21:35:22 +0530

    Delete management directory

--- management/management/src/main/java/com/hospital/management/repository/PatientRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hospital.management.repository;
-
-import com.hospital.management.model.Patient;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PatientRepository extends JpaRepository<Patient, Long> {
-}
